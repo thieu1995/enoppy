cb8b8167b4f92ddee2b37c1651354dc83e183483
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-05-20 16:16:50 +0700

    Add tests

---
+++ tests/test_benchmark.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 20:21, 30/06/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+import numpy as np
+from enoppy.engineer import Engineer
+import pytest
+
+
+def test_Benchmark_class():
+    ndim = 10
+    bounds = np.array([[-15, ]*ndim, [15, ]*ndim]).T
+    problem = Engineer()
+    problem._bounds = bounds
+
+    x = np.random.uniform(problem.lb, problem.ub)
+
+    assert len(problem.lb) == ndim
+    assert isinstance(problem.lb, np.ndarray)
+    assert type(problem.bounds) == np.ndarray
+    assert problem.bounds.shape[0] == ndim
